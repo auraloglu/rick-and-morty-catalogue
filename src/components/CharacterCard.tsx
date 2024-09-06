de78b0c5905d18c1f48f1c4dd78b104ddb130713
Cappydh <ahmeturaloglu@msn.com>
2024-09-06 20:17:58 +0100

    feat: add character card name and image

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -6,7 +6,14 @@ interface CharacterCardProps {
 }
 
 function CharacterCard({ name, image }: CharacterCardProps) {
-  return <div className="character-card">{name}</div>
+  return (
+    <div className="character-card">
+      <div className="character-card-image-container">
+        <img className="character-image" src={image} />
+      </div>
+      <span className="character-name">{name}</span>
+    </div>
+  )
 }
 
 export default CharacterCard
--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -23,7 +23,9 @@ function CharacterList() {
 
   return (
     <>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <div>Character List!</div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {characterList.map((character: any) => {
           return (
             <CharacterCard
