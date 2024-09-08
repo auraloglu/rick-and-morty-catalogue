544fd06a79b803b81abee92b9028532c18533c2f
Cappydh <ahmeturaloglu@msn.com>
2024-09-08 13:24:16 +0100

    feat: add rick and morty font

--- src/components/CharacterDetailCard.tsx
+++ src/components/CharacterDetailCard.tsx
@@ -37,13 +37,23 @@ function CharacterDetailCard({
         <img className="character-image" src={image} />
       </div>
       {isCurrent && (
-        <>
-          <div className="detail status">Status: {status}</div>
-          <div className="detail species">Species: {species}</div>
-          <div className="detail type">Type: {type}</div>
-          <div className="detail gender">Gender: {gender}</div>
-          <div className="detail origin">Origin: {origin}</div>
-        </>
+        <div className="detail-container">
+          <div className="detail status">
+            <label className="label">Status:</label> {status}
+          </div>
+          <div className="detail species">
+            <label className="label">Species:</label> {species}
+          </div>
+          <div className="detail type">
+            <label className="label">Type:</label> {type}
+          </div>
+          <div className="detail gender">
+            <label className="label">Gender:</label> {gender}
+          </div>
+          <div className="detail origin">
+            <label className="label">Origin:</label> {origin}
+          </div>
+        </div>
       )}
     </div>
   )
